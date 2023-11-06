e8f2ed05b8ba5b78c9a9d54b615b36e4166d648a
Linda Vitrella <lvitrella@mail.com>
2023-11-05 20:05:02 -0500

    updated files

---
+++ end.js
@@ -0,0 +1,39 @@
+const username = document.getElementById("username");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
+
+const MAX_HIGH_SCORES = 5;
+console.log(highScore);
+
+//finalScore.innerText = mostRecentScore;
+
+username.addEventListener("keyup", () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+
+saveHighScore = (e) => {
+    console.log("clicked the save button!");
+    e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+
+    highScore.push(score);
+
+    highScore.sort((a, b) => b.score - a.score);
+
+    highScore.splice(5);   
+  //  }); 
+
+  localStorage.setItem("highScore", JSON.stringify(highScore));
+  window.location.assign("/index.html");
+
+   // console.log(highScore);
+};
+
--- quiz.js
+++ quiz.js
@@ -1,8 +1,10 @@
 const question = document.getElementById("question");
 const picks = Array.from(document.getElementsByClassName("pick-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0
 let questionCounter = 0;
 let availableQuestions = [];
@@ -15,7 +17,7 @@ let questions = [
     pick2: "Other arrays",
     pick3: "Booleans",
     pick4: "All of the above",
-    answer: 4,
+    answer: 4
     },
 
     {
@@ -24,7 +26,7 @@ let questions = [
     pick2: "Booleans",
     pick3: "Alerts",
     pick4: "Numbers",
-    answer: 3,
+    answer: 3
     },
 
     {
@@ -33,7 +35,7 @@ let questions = [
     pick2: "Terminal/Bash",
     pick3: "For loops",
     pick4: "Console log",
-    answer: 4,
+    answer: 4
     },
 
     {
@@ -42,7 +44,7 @@ let questions = [
     pick2: "Curly brackets",
     pick3: "Quotes",
     pick4: "Parenthesis",
-    answer: 3,
+    answer: 3
     },
 
     {
@@ -51,7 +53,7 @@ let questions = [
     pick2: "Quotes",
     pick3: "Curly brackes",
     pick4: "Square brackets",
-    answer: 1,
+    answer: 1
     },
 
 ];
@@ -66,36 +68,65 @@ startQuiz = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-   // console.log (availableQuestions);
+  // console.log (availableQuestions);
     getNewQuestion();
 };
 
 getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem("mostRecentScore", score);
+        return window.location.assign("/end.html");
+    }
 
     questionCounter++;
+    questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
    
 
-    picks.forEach((pick) => {
+    picks.forEach(pick => {
         const number = pick.dataset['number'];
-        choice.innerText = currentQuestion["pick" + number];
+        pick.innerText = currentQuestion["pick" + number];
     });
 
+    availableQuestions.splice(questionIndex, 1);
+    console.log(availableQuestions);
     acceptingAnswers = true;
 };
 
-picks.forEach((pick) => {
-    pick.addEventListener('click', (e) => {
+picks.forEach(pick => {
+    pick.addEventListener('click', e => {
         if (!acceptingAnswers) return;
 
-        acceptingAnswers = false;;
+        acceptingAnswers = false;
         const selectedPick = e.target;
         const selectedAnswer = selectedPick.dataset['number'];
+       // console.log(selectedAnswer, currentQuestion.answer);
+       
+
+       const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+       console.log(classToApply);
+
+     if (classToApply === 'correct') {
+         incrementScore(CORRECT_POINTS);
+     } 
+
+       selectedPick.parentElement.classList.add(classToApply);
+
+       setTimeout(() => {
+        selectedPick.parentElement.classList.remove(classToApply);
         getNewQuestion();
+       }, 1000);
+
+       });
     });
-});
+
+    incrementScore = num => {
+        score += num;
+        scoreText.innerText = score;
+    }
 
 startQuiz();
